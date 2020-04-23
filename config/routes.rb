a0c43eefa2bdf7ede8edeb7358d87d628d192d57
Alvaro Alvarez <alvaro.alvarez@2transfair.com>
2020-04-22 20:01:00 -0500

    first step changing js

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'dashboard#index'
 
   get '/dashboard', to: 'dashboard#index'
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: "users/passwords" }
+  devise_scope :user do
+    delete  'logout', to: 'devise/sessions#destroy'
+    get     'logout', to: 'devise/sessions#destroy'
+  end
 end
